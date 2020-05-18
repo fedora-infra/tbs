320997ff2a9cbce7c89921f9510b0b6128102000
Clement Verna <cverna@tutanota.com>
2020-05-18 09:11:45 +0200

    Get the last 100 open tickets for pagure, gitlab and github.
    This commit refactors how we fetch the tickets from pagure, github and gitlab.
    Instead of getting all tickets we care only for the Open tickets. So far we do
    not manage pagination and we only fetch the last 100 open tickets which is in most case
    all of the tickets.
    Also this commit removes the logic to deal with the closed tickets.
    
    Fixes #16
    
    Signed-off-by: Clement Verna <cverna@tutanota.com>

--- tbs.py
+++ tbs.py
@@ -35,11 +35,9 @@ logging.basicConfig()
 logger = logging.getLogger("bugzilla")
 # logger.setLevel(logging.DEBUG)
 
-RETRIES = 2
-
 
 class Project:
-    """ Simple object representation of a project. """
+    """ Simple object representation of a project."""
 
     def __init__(self):
         self.name = ""
@@ -47,11 +45,10 @@ class Project:
         self.url = ""
         self.site = ""
         self.tag = ""
-        self.tickets = []
 
 
 class Ticket:
-    """ Simple object representation of a ticket. """
+    """ Simple object representation of a ticket."""
 
     def __init__(self):
         self.id = ""
@@ -68,8 +65,7 @@ class Ticket:
 
 
 def gather_projects(bz_service=False):
-    """ Retrieve all the projects which have subscribed to this idea.z
-    """
+    """ Retrieve all the projects which have subscribed to this idea."""
     projects_path = "projects.toml"
     if not os.path.exists(projects_path):
         print("No projects file is found")
@@ -85,6 +81,130 @@ def gather_projects(bz_service=False):
     return projects
 
 
+def gather_pagure_tickets(project, all_tickets, state):
+    """Get the last 100 tickets from the specified state on pagure."""
+
+    project.url = "https://pagure.io/%s/" % (project.name)
+    project.site = "pagure.io"
+    url = (
+            "https://pagure.io/api/0/%s/issues"
+            "?status=%s&per_page=100" % (project.name, state.capitalize())
+    )
+    jsonobj = requests.get(url).json()
+    if jsonobj:
+        for ticket in jsonobj["issues"]:
+            ticketobj = Ticket()
+            ticketobj.id = ticket["id"]
+            ticketobj.title = ticket["title"]
+            ticketobj.url = "https://pagure.io/%s/issue/%s" % (
+                project.name,
+                ticket["id"],
+            )
+            ticketobj.labels = ticket["tags"]
+            ticketobj.requester = ticket["user"]["name"]
+            ticketobj.project_url = project.url
+            ticketobj.project = project.name
+            ticketobj.project_site = project.site
+            if ticket["assignee"]:
+                ticketobj.assignee = ticket["assignee"]["fullname"]
+            else:
+                ticketobj.assignee = None
+
+            all_tickets.append(ticketobj)
+
+
+def gather_github_tickets(project, all_tickets, state):
+    """Get the last 100 tickets from the specified state on github."""
+    project.url = "https://github.com/%s/" % (project.name)
+    project.site = "github"
+    url = (
+            "https://api.github.com/repos/%s/issues"
+            "?state=%s&per_page=100" % (project.name, state)
+    )
+    jsonobj = requests.get(url).json()
+    if jsonobj and "pull_request" not in jsonobj:
+        for ticket in jsonobj:
+            ticketobj = Ticket()
+            ticketobj.id = ticket["number"]
+            ticketobj.title = ticket["title"]
+            ticketobj.url = ticket["html_url"]
+            ticketobj.requester = ticket["user"]["login"]
+            ticketobj.project_url = project.url
+            ticketobj.project = project.name
+            ticketobj.project_site = project.site
+            for label in ticket["labels"]:
+                ticketobj.labels.append(label["name"])
+            if ticket["assignee"]:
+                # GitHub api doesn't give full name of the user
+                ticketobj.assignee = ticket["assignee"]["login"]
+            else:
+                ticketobj.assignee = None
+
+            all_tickets.append(ticketobj)
+
+
+def gather_gitlab_tickets(project, all_tickets, state):
+    """Get the last 100 tickets from the specified state on gitlab."""
+    # https://docs.gitlab.com/ee/api/issues.html#list-project-issues
+    project.url = "https://gitlab.com/%s/" % (project.name)
+    project.site = "gitlab.com"
+    url = (
+            "https://gitlab.com/api/v4/projects/%s/issues?state=%s&per_page=100"
+            % (urllib.parse.quote(project.name, safe=""), state)
+    )
+    jsonobj = requests.get(url).json()
+    if jsonobj:
+        for ticket in jsonobj:
+            ticketobj = Ticket()
+            ticketobj.id = ticket["id"]
+            ticketobj.title = ticket["title"]
+            ticketobj.url = ticket["web_url"]
+            ticketobj.labels = ticket["labels"]
+            ticketobj.requester = ""
+            ticketobj.project_url = project.url
+            ticketobj.project = project.name
+            ticketobj.project_site = project.site
+            if ticket["assignee"]:
+                ticketobj.assignee = ticket["assignee"]["name"]
+            else:
+                ticketobj.assignee = None
+
+            all_tickets.append(ticketobj)
+
+
+def gather_bugzilla_tickets(project, all_tickets):
+    """"Get the bugzilla tickets for a component"""
+    project.tag = "bugzillaTag"
+    project.url = "https://bugzilla.redhat.com/buglist.cgi"\
+                  "?bug_status=NEW&bug_status=ASSIGNED&component=%s&product=Fedora"\
+                  % (project.name)
+    project.site = "bugzilla.redhat.com"
+    bz_list = bzclient.query(
+        {
+            "query_format": "advanced",
+            "bug_status": ["NEW", "ASSIGNED"],
+            "classification": "Fedora",
+            "product": "Fedora",
+            "component": project.name
+        })
+    for ticket in bz_list:
+        ticketobj = Ticket()
+        ticketobj.id = ticket.bug_id
+        ticketobj.title = ticket.short_desc
+        ticketobj.url = "https://bugzilla.redhat.com/%s" % (
+            ticket.bug_id)
+        ticketobj.labels = []
+        ticketobj.requester = ticket.creator
+        ticketobj.project_url = project.url
+        ticketobj.project = project.name
+        ticketobj.project_site = project.site
+    if ticket.assigned_to:
+        ticketobj.assignee = ticket.assigned_to
+    else:
+        ticketobj.assignee = None
+    all_tickets.append(ticketobj)
+
+
 def main():
     """ For each projects which have suscribed in the correct place
     (fedoraproject wiki page), gather all the tickets containing the
@@ -98,142 +218,19 @@ def main():
 
     projects = gather_projects()
 
-    ticket_num = 0
     all_tickets = []
     closed_tickets = []
     state = 'open'
     for project in projects:
-        tickets = []
         print(f"Fetching issues for {project.name}")
         if project.service == 'github':
-            project.url = "https://github.com/%s/" % (project.name)
-            project.site = "github"
-            url = (
-                    "https://api.github.com/repos/%s/issues"
-                    "?state=%s&per_page=100" % (project.name, state)
-            )
-            jsonobj = requests.get(url).json()
-            if jsonobj and "pull_request" not in jsonobj:
-                for ticket in jsonobj:
-                    ticket_num = ticket_num + 1
-                    ticketobj = Ticket()
-                    ticketobj.id = ticket["number"]
-                    ticketobj.title = ticket["title"]
-                    ticketobj.url = ticket["html_url"]
-                    ticketobj.requester = ticket["user"]["login"]
-                    ticketobj.project_url = project.url
-                    ticketobj.project = project.name
-                    ticketobj.project_site = project.site
-                    for label in ticket["labels"]:
-                        ticketobj.labels.append(label["name"])
-                    if ticket["assignee"]:
-                        # GitHub api doesn't give full name of the user
-                        ticketobj.assignee = ticket["assignee"]["login"]
-                    else:
-                        ticketobj.assignee = None
-
-                    if ticket["state"] == "closed":
-                        closed_tickets.append(ticketobj)
-                    else:
-                        all_tickets.append(ticketobj)
-
-                    tickets.append(ticketobj)
+            gather_github_tickets(project, all_tickets, state)
         elif project.service == "pagure":
-            project.url = "https://pagure.io/%s/" % (project.name)
-            project.site = "pagure.io"
-            url = (
-                    "https://pagure.io/api/0/%s/issues"
-                    "?status=%s&per_page=100" % (project.name, state.capitalize())
-            )
-            jsonobj = requests.get(url).json()
-            if jsonobj:
-                for ticket in jsonobj["issues"]:
-                    ticket_num = ticket_num + 1
-                    ticketobj = Ticket()
-                    ticketobj.id = ticket["id"]
-                    ticketobj.title = ticket["title"]
-                    ticketobj.url = "https://pagure.io/%s/issue/%s" % (
-                        project.name,
-                        ticket["id"],
-                    )
-                    ticketobj.labels = ticket["tags"]
-                    ticketobj.requester = ticket["user"]["name"]
-                    ticketobj.project_url = project.url
-                    ticketobj.project = project.name
-                    ticketobj.project_site = project.site
-                    if ticket["assignee"]:
-                        ticketobj.assignee = ticket["assignee"]["fullname"]
-                    else:
-                        ticketobj.assignee = None
-
-                    if ticket["status"] == "Closed":
-                        closed_tickets.append(ticketobj)
-                    else:
-                        all_tickets.append(ticketobj)
-
-                    tickets.append(ticketobj)
-
+            gather_pagure_tickets(project, all_tickets, state)
         elif project.service == "gitlab":
-            # https://docs.gitlab.com/ee/api/issues.html#list-project-issues
-            project.url = "https://gitlab.com/%s/" % (project.name)
-            project.site = "gitlab.com"
-            url = (
-                    "https://gitlab.com/api/v4/projects/%s/issues"
-                    "?state=%s&labels=%s"
-                    % (urllib.parse.quote(project.name, safe=""), state, label)
-            )
-            jsonobj = requests.get(url).json()
-            if jsonobj:
-                for ticket in jsonobj:
-                    ticket_num = ticket_num + 1
-                    ticketobj = Ticket()
-                    ticketobj.id = ticket["id"]
-                    ticketobj.title = ticket["title"]
-                    ticketobj.url = ticket["web_url"]
-                    ticketobj.labels = label
-                    ticketobj.requester = ""
-                    ticketobj.project_url = project.url
-                    ticketobj.project = project.name
-                    ticketobj.project_site = project.site
-                    if ticket["assignee"]:
-                        ticketobj.assignee = ticket["assignee"]["name"]
-                    else:
-                        ticketobj.assignee = None
-                    tickets.append(ticketobj)
-                    all_tickets.append(ticketobj)
+            gather_gitlab_tickets(project, all_tickets, state)
         elif project.service == "bugzilla":
-            project.tag = "bugzillaTag"
-            project.url = "https://bugzilla.redhat.com/buglist.cgi"\
-                          "?bug_status=NEW&bug_status=ASSIGNED&component=%s&product=Fedora"\
-                          % (project.name)
-            project.site = "bugzilla.redhat.com"
-            bz_list = bzclient.query(
-                {
-                    "query_format": "advanced",
-                    "bug_status": ["NEW", "ASSIGNED"],
-                    "classification": "Fedora",
-                    "product": "Fedora",
-                    "component": project.name
-                })
-            for ticket in bz_list:
-                ticket_num = ticket_num + 1
-                ticketobj = Ticket()
-                ticketobj.id = ticket.bug_id
-                ticketobj.title = ticket.short_desc
-                ticketobj.url = "https://bugzilla.redhat.com/%s" % (
-                    ticket.bug_id)
-                ticketobj.labels = []
-                ticketobj.requester = ticket.creator
-                ticketobj.project_url = project.url
-                ticketobj.project = project.name
-                ticketobj.project_site = project.site
-            if ticket.assigned_to:
-                ticketobj.assignee = ticket.assigned_to
-            else:
-                ticketobj.assignee = None
-            tickets.append(ticketobj)
-            all_tickets.append(ticketobj)
-        project.tickets = tickets
+            gather_bugzilla_tickets(project, all_tickets)
 
     tickets_groomed = []
     tickets_in_progress = []
@@ -258,7 +255,6 @@ def main():
         html = mytemplate.render(
             projects=projects,
             all_tickets=all_tickets,
-            ticket_num=ticket_num,
             tickets_groomed=tickets_groomed,
             tickets_in_progress=tickets_in_progress,
             tickets_untaged=tickets_untaged,
